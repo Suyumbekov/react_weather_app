386e6c3252d4add70eb8544ba7862ae122f70e3c
Suyumbekov <tema.sebat@gmail.com>
2023-07-26 04:02:19 +0600

    initial commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "24b7d64d4147d7987769f72d9700a97e"
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5824a4de02msh99bee112e4fe5acp184b08jsn3cec82129b20',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
